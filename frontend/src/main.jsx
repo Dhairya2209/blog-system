d0b9602c8e88d05be69a3d37ac245db6d7f34eb9
Dhairya <pmdhairya56@gmail.com>
2026-02-06 23:45:38 +0530

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/app.css';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
